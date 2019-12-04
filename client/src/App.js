f97c571cd50b54493510adbce16d5c8cf9e2bd72
Janessa Garrow <janessa.garrow@gmail.com>
2019-12-03 22:43:51 -0700

    Added  functionality

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,22 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+    const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+    const addToSavedList = movie => {
+        setSavedList([...savedList, movie]);
+    };
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:id" component={Movie} />
-    </div>
-  );
+    return (
+        <div>
+            <SavedList list={savedList} />
+            <Route exact path="/" component={MovieList} />
+            <Route
+                path="/movies/:id"
+                render={props => <Movie addToSavedList={addToSavedList} {...props} />}
+            />
+        </div>
+    );
 };
 
 export default App;
